8e48e84c7d71ef565d7caf96aa90b17b7727e2bc
kmarks2013 <kmarks2013@gmail.com>
2019-10-17 18:35:11 -0400

    generated resources for doctor model

---
+++ db/migrate/20191017211258_create_doctors.rb
@@ -0,0 +1,12 @@
+class CreateDoctors < ActiveRecord::Migration[6.0]
+  def change
+    create_table :doctors do |t|
+      t.string :name
+      t.string :image
+      t.string :bio
+      t.string :wikiLink
+
+      t.timestamps
+    end
+  end
+end

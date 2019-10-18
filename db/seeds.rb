9aec744eb02299e57b67f413fd0f490ee95b7a5d
kmarks2013 <kmarks2013@gmail.com>
2019-10-18 15:16:53 -0400

    added initalizers and updated seeds to show images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 
+Doctor.destroy_all
 
-
-twelfth = Doctor.create(name: "The Twelfth Doctor", image:"public/doctorImages/twelfth-doctor.png", bio:"This Doctor is notable for being the first incarnation of the doctors second regeneration cycle.", wikilink:"https://tardis.fandom.com/wiki/Twelfth_Doctor")
-thriteenth = Doctor.create(name: "The Doctor", image: "public/doctorImages/thirteenth-doctor.png", bio:"The current incarnation of teh Doctor. Endlessly Kind and filled with hope, she is curretnly traveling with her comapnions.", wikilink:"https://tardis.fandom.com/wiki/Thirteenth_Doctor" )
+twelfth = Doctor.create(name: "The Twelfth Doctor", image:"https://www.pngkit.com/png/full/211-2115843_twelfth-doctor-doctor-who-12th-doctor-png.png", bio:"This Doctor is notable for being the first incarnation of the doctors second regeneration cycle.", wikilink:"https://tardis.fandom.com/wiki/Twelfth_Doctor")
+thriteenth = Doctor.create(name: "The Doctor", image: "https://www.pngkit.com/png/full/36-362232_thirteenth-doctor-dr-who-jodie-whittaker.png", bio:"The current incarnation of teh Doctor. Endlessly Kind and filled with hope, she is curretnly traveling with her comapnions.", wikilink:"https://tardis.fandom.com/wiki/Thirteenth_Doctor" )
